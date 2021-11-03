42947ffd81a28edae885d8c63c00f7a49ba2f865
Евгения <Jenua.p17@gmail.com>
2021-11-03 20:27:38 +0300

    Созданы 4 класса:
    1. Car (Автомобиль)
    2. Brand (Бренд)
    3. Worker (Сотрудник)
    4. Carsale (Продажа автомобиля)

--- dealership-practice/Program.cs
+++ dealership-practice/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 
-namespace dealership_practice
+namespace dealershippractice
+   
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var car = new Domain.Car(1, 2, "BMV-iX", 2021, 1.2, "blue", 1200000.00);
+            var brand = new Domain.Brand(1, "Volvo", "Sweden");
+            var carsale = new Domain.Carsale(3, 1, 2, DateTime.Today, 1300000, 1);
+            var woker = new Domain.Woker(6, "Jon Smitt", "seller");
+            Console.WriteLine($"car={car} brand= {brand} carsale={carsale} woker={woker}");
         }
     }
 }
